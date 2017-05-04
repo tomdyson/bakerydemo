a111716ffe89d70a7ecde6370437aa18887199ca
Tom Dyson <tom@torchbox.com>
2017-05-04 16:38:50 +0100

    Stub entity extraction function

---
+++ bakerydemo/base/utils.py
@@ -0,0 +1,3 @@
+def extract_entities(text, salience_threshold=0.15, max_results=50):
+    "extract entities from plain text, whose salience exceeds the threshold"
+    return ["Wagtail", "Torchbox", "Python", "Django"]
